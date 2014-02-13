490f3207d3c3f7453190d64546585bf93bdce9cd
Megan Willis <m.willis@ry.com>
2014-02-13 14:15:56 +0000

    Catousel: Part 1

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,9 @@
 $(document).ready(function() {
 	
 	$("ul#catList li").click(yeahtheyare);
+
+	var carousel = new Carousel($("#catousel"));
+
 });
 
 function yeahtheyare()
@@ -9,3 +12,16 @@ function yeahtheyare()
 	var bigalert = $(this).text();
 	alert(bigalert + " YEAH THEY ARE!");
 }
+
+function Carousel($catousel)
+{
+	//Finds width of container
+	var width = $catousel.width();
+	//finds number of slides
+	var numberofslides = $catousel.find("li").length;
+	//multiplies width of container with number of slides
+	var ulwidth = width*numberofslides;
+	//uses ulwidth to set width of carousel
+	$catousel.find("ul").width(ulwidth);
+}
+
